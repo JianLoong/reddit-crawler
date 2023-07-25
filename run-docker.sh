61f240e3a5325a79f38f68a10adae44ff70d59e9
Jian Liew <jianloongliew@gmail.com>
2023-07-25 20:57:55 +1000

    Updated

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+cd /home/jian/reddit-crawler
 docker compose -f /home/jian/reddit-crawler/docker-compose.yml up
+git add .
+git commit -m "Updated"
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa -F /dev/null" git push
